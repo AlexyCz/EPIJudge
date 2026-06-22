d360b29578c152fc1a5f658464a7b722efccd9b0
Alexy <alexycrz@gmail.com>
2026-06-22 17:02:06 -0500

    Passes problem 12.1; Updates scenerio tracker.

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,8 +1,40 @@
+import collections
+
 from test_framework import generic_test
 
 
 def can_form_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
+    """
+    Write a program to test whether the letters forming a string can be permuted
+        to form a palindrome.
+
+    have: a string named `s`
+
+    logic:
+        * quantify character counts
+            * can use collections.Counter()
+        * for a string to be permutable to a palindrome, we need all evens
+            or all evens and a single odd. The odd satisfies the single middle
+            character.
+        * we instantiate an `odd` flag to initialize as false, and set to true
+            the moment we encounter an odd count character from the dictionary.
+            * if we encounter another odd, we return False.
+        * we loop throught the hash items, and modulo counts with 2 for even value
+            validation.
+        * return true if we loop through everything.
+    """
+    odd_count_char = False
+    character_counts = collections.Counter(s)
+
+    for _, count in character_counts.items():
+        parity = count % 2
+
+        if odd_count_char and parity:
+            return False
+
+        if parity and not odd_count_char:
+            odd_count_char = True
+
     return True
 
 
