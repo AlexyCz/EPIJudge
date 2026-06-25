873b62fea9d2c4804a9ad672ee977aa8e32e0b45
Alexy <alexycrz@gmail.com>
2026-06-25 14:20:04 -0500

    Passes 10.1; Fails 5.12; updates tracker.

--- epi_judge_python/offline_sampling.py
+++ epi_judge_python/offline_sampling.py
@@ -1,4 +1,6 @@
 import functools
+import random
+import time
 from typing import List
 
 from test_framework import generic_test
@@ -9,8 +11,22 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def random_sampling(k: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    """
+        Implement algorithm that takes as input an array of distinct elements 
+            and a size, and returns a subset of the given size of the array 
+            of elements. All subsets should be equally likely.
+        Return the result in input array itself.
+    """
+    random.seed(time.monotonic_ns())
+    chosen_idx = set()
+
+    while len(chosen_idx) < k+1:
+        chosen_idx.add(random.randrange(0, len(A)))
+
+    for idx, choice in enumerate(chosen_idx):
+        A[idx], A[choice] = A[choice], A[idx]
+
+    return 
 
 
 @enable_executor_hook
--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,11 +1,41 @@
+import heapq
 from typing import List
 
 from test_framework import generic_test
 
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """
+        Trade is encoded as: 123111, AAPL, 30, 456.12
+        trade[0] - milliseconds since start of day.
+        trade[1] - symbol
+        trade[2] - share number
+        trade[3] - price
+
+    Write a program that takes as input a set of sorted sequences and 
+        computes the union of these sequences as a sorted sequence.
+        e.g. [3,5,7], [0,6], [0,6,28] -> [0,0,3,5,6,6,7,28]
+
+    have: a list of lists containing numbers; already sorted.
+
+    logic:
+        * use a heap (heapq.heapify(iterable)) initialized at empty.
+        * linear traversal
+        * each item encountered will be passed into heappush(),
+            along with our result heap.
+        * return result
+    """ 
+    
+    # return list(heapq.merge(*sorted_arrays))  # pythonic.
+    merged_sequences = list()
+    heapq.heapify(merged_sequences)
+
+    for seq in sorted_arrays:
+        for n in seq:
+            heapq.heappush(merged_sequences, n)
+
+    return heapq.nsmallest(len(merged_sequences), merged_sequences)
+    
 
 
 if __name__ == '__main__':
