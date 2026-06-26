cb0b4e6791f907dc94b2962acc6c95aa2accb441
Alexy <alexycrz@gmail.com>
2026-06-26 16:39:08 -0500

    Attempted problem 9.4; Updates scenerio tracker.

--- epi_judge_python/lowest_common_ancestor_with_parent.py
+++ epi_judge_python/lowest_common_ancestor_with_parent.py
@@ -10,8 +10,45 @@ from test_framework.test_utils import enable_executor_hook
 
 def lca(node0: BinaryTreeNode,
         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+    """
+        Given two nodes in a binary tree, design an algorithm that computes
+            their lca; assume that each node has a parent pointer.
+
+        have:
+            node 0 and node 1 of type BinaryTreeNode with value, left, right, parent attrs.
+
+        logic:
+            * base case being if either is the root where parent is none will be the lca.
+            ---
+            * we have a hint of depth providing easy backtrack to lca via parent pointer.
+            ---
+            * idea! depth-first to find shortest path to node0 and node1.
+                * the trail to one will stop overlapping with the other the moment we drop
+                    into the node right after the lca.
+                * space is at most O(k) where k is depth of tree.
+    """
+    trail = list()
+
+    def depth_first_trail_from(node: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+        nonlocal trail
+        print(f"\n node:{node}, trail:{trail}")
+
+        if not node:
+            return
+
+        if node.data in trail:
+            return node
+
+        trail.append(node.data)
+        _ = depth_first_trail_from(node.parent)
+        
+        return
+
+    _ =  depth_first_trail_from(node0)
+
+    result = depth_first_trail_from(node1)
+
+    return result
 
 
 @enable_executor_hook
