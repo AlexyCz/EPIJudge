e80486d345b1e90383ba5336bf2190b2da372976
Alexy <alexycrz@gmail.com>
2026-07-01 21:10:54 -0500

    Passes 8.1; updates tracker.

--- epi_judge_python/stack_with_max.py
+++ epi_judge_python/stack_with_max.py
@@ -3,20 +3,55 @@ from test_framework.test_failure import TestFailure
 
 
 class Stack:
+    """
+        Design a stack that includes a max operation, in addition to push and pop.
+        The max method should return the maximum value stored in the stack.
+
+        Have:
+            * Four methods and their definitions:
+                empty
+                max
+                pop
+                push
+
+        Logic:
+            * empty:
+                Create length attribute, reference and use implicit boolean.
+            * max:
+                Create a second stack attr to keep track of max of each state
+                    in stack.
+            * pop:
+                Built in list type pop method, i.e. pop() for main stack.
+                We'll also pop from max stack attr to keep consistent state.
+            * push:
+                Build in list type append method, for main stack.
+                We'll also append the max of current value and latest max stack value.
+    """
+    def __init__(self) -> None:
+        self.main_stack = []
+        self.max_stack = []
+        self.length = 0
+        
     def empty(self) -> bool:
-        # TODO - you fill in here.
-        return True
+        return False if self.length else True
 
     def max(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        return self.max_stack[-1]
 
     def pop(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        result = self.main_stack.pop()
+        _ = self.max_stack.pop()
+        self.length -= 1
+        return result
 
     def push(self, x: int) -> None:
-        # TODO - you fill in here.
+        self.main_stack.append(x)
+        self.length += 1
+        try:
+            new_max = max(self.max_stack[-1], x)
+            self.max_stack.append(new_max)
+        except IndexError:
+            self.max_stack.append(x)
         return
 
 
